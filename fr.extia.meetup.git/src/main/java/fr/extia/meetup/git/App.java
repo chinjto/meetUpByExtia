8372aa2c74bb0eea55924aa0f077e56661186ee2
chinjto <cyril_defaye@yahoo.fr>
2019-12-10 19:14:33 +0100

    Initialisation du projet

---
+++ fr.extia.meetup.git/src/main/java/fr/extia/meetup/git/App.java
@@ -0,0 +1,13 @@
+package fr.extia.meetup.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
